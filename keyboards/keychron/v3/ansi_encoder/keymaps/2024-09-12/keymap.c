128b5e5a32ed348fccb6c63dce6192f455166ba9
Ryan Copenhaver <copenhaverr@ufl.edu>
2024-09-12 21:22:11 -0400

    updated L1 and NUM layers on v3 max

--- keyboards/keychron/v3/ansi_encoder/keymaps/2024-09-12/keymap.c
+++ keyboards/keychron/v3/ansi_encoder/keymaps/2024-09-12/keymap.c
@@ -46,21 +46,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // L1 - Arrows
     [L1] = LAYOUT_tkl_f13_ansi(
         _______,  RGB_TOG,  RGB_MOD,  RGB_RMOD, RGB_VAI,  RGB_VAD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_SPI,  RGB_SPD,  _______,     TG(FAM),  _______,  _______,  _______, // F Row
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  // Numbers
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,  _______,  _______,  _______,  // Numbers
         KC_LCAP,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPLY,  KC_MPRV,  KC_MNXT,  KC_PGUP,  KC_UP,  KC_PGDOWN,  KC_PSCR,  KC_INS,  KC_SCRL,    KC_PAUSE,  _______,  _______,  _______,  // QWERTY
-        _______,  KC_LALT,  KC_LCTL,  KC_LSFT,  MO(NUM),  _______,  _______,  KC_LEFT,  KC_DOWN, KC_RIGHT,  KC_BSPC,  KC_DEL,                        _______,                      // Enter Row ASDF
-        _______,            _______,  _______,  _______,  _______,  _______,  KC_PGDN,  KC_APP,  _______,  _______,  _______,              _______,            _______,            // Shift Row ZXCV
-        _______,  _______,  _______,                                _______,                                _______,  _______,  MO(NUM),    _______,  _______,  _______,  _______), // Mod Row
+        _______,  KC_LALT,  KC_LCTL,  KC_LSFT,  MO(NUM),  _______,  KC_HOME,  KC_LEFT,  KC_DOWN, KC_RIGHT,  KC_BSPC,  KC_DEL,                        _______,                      // Enter Row ASDF
+        _______,            _______,  _______,  _______,  _______,  _______,  KC_END,  KC_APP,  _______,  _______,  _______,              _______,            _______,            // Shift Row ZXCV
+        _______,  _______,  _______,                                _______,                                _______,  _______,  MO(MAC),    _______,  _______,  _______,  _______), // Mod Row
     
-    // TODO: Implement!
     // NUM - Numpad
     [NUM] = LAYOUT_tkl_f13_ansi(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,  KC_PSCR,  KC_NO,    RGB_MOD, // F Row
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  // Numbers
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  // QWERTY
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                 // Enter Row ASDF
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,              // Shift Row ZXCV
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT), // Mod Row
+        KC_ESC,   _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,   _______,   _______,   _______,  _______,  _______,    _______, // F Row
+        _______,   ______,  _______,  _______,    _______,     _______,    _______,  KC_7,     KC_8,     KC_9, _______,  _______, _______, _______, _______,  _______,  _______,  // Numbers
+        _______,   _______,  _______,  _______,   _______,  _______,  _______,  KC_4,     KC_5,     KC_6,     _______,   _______, _______, _______,  _______, _______,  _______,  // QWERTY
+        _______,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,  _______,  KC_1,     KC_2,     KC_3,     KC_BSPC,  KC_DEL,              KC_ENT,                                 // Enter Row ASDF
+        _______,            _______,  _______,   _______,  _______, _______, KC_0,     KC_0,     KC_COMM,  KC_DOT,   KC_SLSH,              _______,            KC_UP,              // Shift Row ZXCV
+        _______,  _______,  _______,                                _______,                                 _______,  _______,  _______, _______,  KC_LEFT,  KC_DOWN,  KC_RGHT), // Mod Row
 
     // TODO: Implement!
     // MAC - Macros
